57ad379e6dc2478fdf0b47edd555351e323e9709
edoxp1 <50281390+edoxp1@users.noreply.github.com>
2020-10-18 18:42:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,7 +89,7 @@ client.unload = command => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
-      msg.reply('Aleyküm Selam Gardaş !');
+      msg.reply('Aleyküm Selam ');
 }
 });
 
